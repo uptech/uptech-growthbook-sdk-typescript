4211f21c6883889c878b1d7bbf510c5c1a4a381b
Sierra Henderson <henderson.sierra@yahoo.com>
2023-01-11 13:23:28 -0800

    [UE-5] Init and isOn functions for wrapper
    The intention for this change is to add the init and initForTest
    functions that make this wrapper functional, along with a helper
    method to create the Growthbook client instance. Since the test init
    function is not actually making a call to the api, the fetch function
    is omitted from the method. The seedsToGBFeatures method formats
    the passed in seeds to match with the types returned from Growthbook
    when we fetch from the API.  The isOn method is also added to
    give important functionality as well as an easy method to use in
    testing as more features are added.
    
    ps-id: F9AB1E3E-C8C8-41D0-AD2F-F55312FC2173

--- src/uptech-growthbook-wrapper.test.ts
+++ src/uptech-growthbook-wrapper.test.ts
@@ -1,10 +1,34 @@
-import { UptechGrowthBookTypescriptWrapper } from "./uptech-growthbook-wrapper";
+import { UptechGrowthBookTypescriptWrapper } from './uptech-growthbook-wrapper';
+
+class ToglTest extends UptechGrowthBookTypescriptWrapper {
+
+  static instance: ToglTest = new ToglTest('https://cdn.growthbook.io/api/features/dummy-api-key');
+}
 
 describe("Uptech Growthbook Wrapper", () => {
-    describe("isAwesome", () => {
-        it("should return true", () => {
-            const wrapper = new UptechGrowthBookTypescriptWrapper()
-            expect(wrapper.isAwesome).toEqual(true);
+    describe("isOn", () => {
+        describe("when a feature value is present", () => {
+          beforeEach(() => {
+            ToglTest.instance.initForTests({
+              'some-feature-name': true,
+            });
           });
+  
+          it("should return true", () => {
+              expect(ToglTest.instance.isOn('some-feature-name')).toEqual(true);
+          });
+        });
+
+        describe("when a feature value is not present", () => {
+          beforeEach(() => {
+            ToglTest.instance.initForTests({
+              'some-feature-name': true,
+            });
+          });
+  
+          it("should return true", () => {
+              expect(ToglTest.instance.isOn('some-other-name')).toEqual(false);
+          });
+        });
     });
   });
