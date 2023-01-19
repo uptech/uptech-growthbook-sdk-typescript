2879f39f789e311e5ebc3339e9b5957b4069d107
Drew De Ponte <cyphactor@gmail.com>
2023-01-19 15:53:32 -0500

    Switch from nodejs 18 fetch to cross-fetch
    Switch from nodejs 18 fetch to cross-fetch becaues it seems that nodejs
    18 fetch isn't actually there even though they say it should be.
    
    [changelog]
    fixed: ReferenceError fetch is not defined when refreshing
    
    skip_ticket_id_check
    
    <!-- ps-id: d952bfcd-bd5f-4b33-9b36-3d47d5e11964 -->

--- src/uptech-growthbook-wrapper.ts
+++ src/uptech-growthbook-wrapper.ts
@@ -1,132 +1,133 @@
 import { FeatureDefinition, GrowthBook } from "@growthbook/growthbook";
+import fetch from 'cross-fetch';
 
 interface InitParameters {
-    seeds?: Map<string, any>,
-    overrides?: Map<string, any>,
-    attributes?: Map<string, any>,
-    rules?: Array<Record<string, any>>,
-}   
+  seeds?: Map<string, any>,
+  overrides?: Map<string, any>,
+  attributes?: Map<string, any>,
+  rules?: Array<Record<string, any>>,
+}
 
 export class UptechGrowthBookTypescriptWrapper {
-    constructor(apiUrl: string, getEnv: (key: string) => string | undefined = (key) => process.env[key]) {
-        this.url = apiUrl;
-        this.getEnv = getEnv;
+  constructor(apiUrl: string, getEnv: (key: string) => string | undefined = (key) => process.env[key]) {
+    this.url = apiUrl;
+    this.getEnv = getEnv;
+  }
+  private client: GrowthBook;
+  private readonly url: string;
+  private overrides: Map<string, any> = new Map();
+  private attributes: Map<string, any> = new Map();
+  getEnv: (key: string) => string;
+
+  public async init(
+    { seeds, overrides, attributes }: InitParameters
+  ): Promise<void> {
+    this.overrides.clear();
+    if (overrides != null) {
+      this.overrides = overrides;
     }
-    private client: GrowthBook;
-    private readonly url: string;
-    private overrides: Map<string, any> = new Map();
-    private attributes: Map<string, any> = new Map();
-    getEnv: (key: string) => string;
-
-    public async init(
-        {seeds, overrides, attributes}: InitParameters
-    ): Promise<void> {
-        this.overrides.clear();
-        if (overrides != null) {
-            this.overrides = overrides;
-        }
-        this.attributes.clear();
-        if (attributes != null) {
-            this.attributes = attributes;
-        }
-        this.client = this.createClient(seeds);
-        await this.refresh();
+    this.attributes.clear();
+    if (attributes != null) {
+      this.attributes = attributes;
     }
+    this.client = this.createClient(seeds);
+    await this.refresh();
+  }
 
-    public initForTests({seeds, overrides, attributes, rules}: InitParameters
-    ): void {
-        this.overrides.clear();
-        if (overrides != null) {
-            this.overrides = overrides;
-        }
-        this.attributes.clear();
-        if (attributes != null) {
-            this.attributes = attributes;
-        }
-        this.client = this.createClient(seeds, rules);
+  public initForTests({ seeds, overrides, attributes, rules }: InitParameters
+  ): void {
+    this.overrides.clear();
+    if (overrides != null) {
+      this.overrides = overrides;
     }
-
-    /// Force a refresh of toggles from the server
-    public async refresh(): Promise<void> {
-        try {
-            const growthbookResult = await fetch(this.url)
-            const growthbook = await growthbookResult.json();
-            this.client.setFeatures(growthbook.features);
-        } catch(e) {
-            console.log({
-                message: 'Error while fetching features from GrowthBook',
-                e
-              });
-        }
+    this.attributes.clear();
+    if (attributes != null) {
+      this.attributes = attributes;
     }
+    this.client = this.createClient(seeds, rules);
+  }
 
-    public setAttributes(attributes: Map<string, any>): void {
-        this.client.setAttributes(this.getGBAttributes(attributes));
+  /// Force a refresh of toggles from the server
+  public async refresh(): Promise<void> {
+    try {
+      const growthbookResult = await fetch(this.url)
+      const growthbook = await growthbookResult.json();
+      this.client.setFeatures(growthbook.features);
+    } catch (e) {
+      console.log({
+        message: 'Error while fetching features from GrowthBook',
+        e
+      });
     }
+  }
 
-    /// Check if a feature is on/off
-    public isOn(featureId: string): boolean {
-        const hasOverride = this.overrides.has(featureId);
+  public setAttributes(attributes: Map<string, any>): void {
+    this.client.setAttributes(this.getGBAttributes(attributes));
+  }
 
-        if (hasOverride) {
-            const value = this.overrides.get(featureId);
+  /// Check if a feature is on/off
+  public isOn(featureId: string): boolean {
+    const hasOverride = this.overrides.has(featureId);
 
-            return Boolean(value);
-        } else {
-            const overrideKey = this.getOverrideKeyById(featureId);
-            const featureOverride = this.getEnv(overrideKey);
-            if (featureOverride != null && featureOverride != undefined) {
-                return featureOverride == 'false' ? false : true ;
-            }
-            return this.client.feature(featureId).on ?? false;
-        }
-    }
+    if (hasOverride) {
+      const value = this.overrides.get(featureId);
 
-    /// Return the value of a feature.
-    /// If the feature does not have a value configured, returns null.
-    public value(featureId: string): any {
-      const hasOverride = this.overrides.has(featureId);
-    
-      if (hasOverride) {
-        return this.overrides.get(featureId);
+      return Boolean(value);
+    } else {
+      const overrideKey = this.getOverrideKeyById(featureId);
+      const featureOverride = this.getEnv(overrideKey);
+      if (featureOverride != null && featureOverride != undefined) {
+        return featureOverride == 'false' ? false : true;
       }
-    
-      return this.client.getFeatureValue(featureId, null);
+      return this.client.feature(featureId).on ?? false;
+    }
+  }
+
+  /// Return the value of a feature.
+  /// If the feature does not have a value configured, returns null.
+  public value(featureId: string): any {
+    const hasOverride = this.overrides.has(featureId);
+
+    if (hasOverride) {
+      return this.overrides.get(featureId);
     }
 
-    private getOverrideKeyById(featureId: string): string {
-        const togl = 'TOGL_'
-        return togl + featureId.toLocaleUpperCase().replace(/-/g, '_')
-    }
+    return this.client.getFeatureValue(featureId, null);
+  }
 
-    private createClient(seeds: Map<string, any>, rules?: Array<Record<string, any>>): GrowthBook {
-        return new GrowthBook({
-            enabled: true,
-            qaMode: false,
-            trackingCallback: (gbExperiment, gbExperimentResult) => {},
-            attributes: this.getGBAttributes(this.attributes),
-            features: this.seedsToGBFeatures(seeds, rules),
-        });
-    }
-    private seedsToGBFeatures(seeds: Map<string, any>, rules?: Array<Record<string, any>>): Record<string, FeatureDefinition> {
-        if (seeds == null) {
-            return {};
-        }
-        const features = {};
-        seeds.forEach((value, key) => {
-            features[key] = rules ? { defaultValue: value, rules } : { defaultValue: value };
-        })
-        return features;
-    }
+  private getOverrideKeyById(featureId: string): string {
+    const togl = 'TOGL_'
+    return togl + featureId.toLocaleUpperCase().replace(/-/g, '_')
+  }
 
-    private getGBAttributes(attributes: Map<string, any>): Record<string, any> {
-        if (attributes == null) {
-            return {};
-        }
-        const features = {};
-        attributes.forEach((value, key) => {
-            features[key] = value;
-        })
-        return features;
+  private createClient(seeds: Map<string, any>, rules?: Array<Record<string, any>>): GrowthBook {
+    return new GrowthBook({
+      enabled: true,
+      qaMode: false,
+      trackingCallback: (gbExperiment, gbExperimentResult) => { },
+      attributes: this.getGBAttributes(this.attributes),
+      features: this.seedsToGBFeatures(seeds, rules),
+    });
+  }
+  private seedsToGBFeatures(seeds: Map<string, any>, rules?: Array<Record<string, any>>): Record<string, FeatureDefinition> {
+    if (seeds == null) {
+      return {};
     }
+    const features = {};
+    seeds.forEach((value, key) => {
+      features[key] = rules ? { defaultValue: value, rules } : { defaultValue: value };
+    })
+    return features;
+  }
+
+  private getGBAttributes(attributes: Map<string, any>): Record<string, any> {
+    if (attributes == null) {
+      return {};
+    }
+    const features = {};
+    attributes.forEach((value, key) => {
+      features[key] = value;
+    })
+    return features;
+  }
 }
